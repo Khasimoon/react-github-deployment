6a4aa4d00d6ad99aaf1f339354b40aa7eb168c2b
Khasimoon <skkhasimoon@gmail.com>
2023-05-16 00:51:33 +0530

    version 2 created

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-    Github Deploment..
+    Github Deploment version 2
         </p>
         <a
           className="App-link"
